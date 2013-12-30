dc4e92b611fc3bae24fbb38de360fc0653e51317
mehulkar <mehul.kar@gmail.com>
2013-12-29 22:02:30 -0600

    Put news feed on home page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,1 +1,10 @@
-export default Ember.Route.extend();
+export default Ember.Route.extend({
+  setupController: function() {
+    return this.controllerFor('newsList').set('content', this.store.find('bulletin'));
+  },
+  actions: {
+    goToBulletin: function(bulletin) {
+      return this.transitionTo('news.story', bulletin);
+    }
+  }
+});
