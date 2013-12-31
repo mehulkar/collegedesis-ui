0910ad8deac6e8a0749a353e2d0bc1ca0baefa31
mehulkar <mehul.kar@gmail.com>
2013-12-31 02:45:13 -0800

    computed.not

--- app/models/membership.js
+++ app/models/membership.js
@@ -4,14 +4,14 @@ export default DS.Model.extend({
   approved: DS.attr('boolean'),
   displayName: DS.attr('string'),
   membershipTypeId: DS.attr('number'),
-  notApproved: (function() {
-    return !this.get('approved');
-  }).property('approved'),
-  loading: (function() {
+
+  notApproved: Em.computed.not('approved'),
+
+  loading: function() {
     if (this.get('isLoaded') && this.get('organization.isLoaded')) {
       return false;
     } else {
       return true;
     }
-  }).property('isLoaded', 'organization.isLoaded')
+  }.property('isLoaded', 'organization.isLoaded')
 });
