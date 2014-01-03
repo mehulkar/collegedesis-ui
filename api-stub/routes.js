61eddae2fb3e42a816097fb5d6d2df0753a9bf85
mehulkar <mehul.kar@gmail.com>
2014-01-03 02:22:51 -0800

    Update EAK to include config directory

--- api-stub/routes.js
+++ api-stub/routes.js
@@ -1,6 +1,6 @@
 module.exports = function(server) {
 
-  // Create an API namespace, so that the root does not 
+  // Create an API namespace, so that the root does not
   // have to be repeated for each end point.
 	server.namespace('/api', function() {
 
