a55979977a387a6b6a52e8b05058f7955b1ba5d3
mehulkar <mehul.kar@gmail.com>
2014-02-06 00:58:20 +0530

    Add imageURL to comment authors

--- app/models/comment.js
+++ app/models/comment.js
@@ -2,5 +2,6 @@ export default DS.Model.extend({
   body: DS.attr('string'),
   bulletin: DS.belongsTo('bulletin'),
   author: DS.attr('string'),
+  user: DS.belongsTo('user'),
   createdAt: DS.attr('date')
 });
