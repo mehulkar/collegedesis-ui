f5d62db299fb7967992c3f3bcabc37820cc67a21
mehulkar <mehul.kar@gmail.com>
2014-02-09 06:32:58 -0800

    include markdown manually in app instead of through vendor

--- app/helpers/markdown-text.js
+++ app/helpers/markdown-text.js
@@ -2,7 +2,9 @@
 // https://github.com/evilstreak/markdown-js
 // bower install markdown --save
 
+var markdown = require('collegedesis/markdown');
+
 export default Ember.Handlebars.makeBoundHelper(function(value) {
   if (Ember.isEmpty(value)) return "";
-  return new Ember.Handlebars.SafeString(markdown.toHTML(value));
+  return new Ember.Handlebars.SafeString(window.markdown.toHTML(value));
 });
