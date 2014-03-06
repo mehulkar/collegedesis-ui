fc95d5b7ac270527187fb1fd72f6480ef8bb2a57
mehulkar <mehul.kar@gmail.com>
2014-03-05 22:46:35 -0800

    remove comments

--- config/application.js
+++ config/application.js
@@ -9,35 +9,3 @@
 //= require_tree ./initializers
 
 window.App = require('app').default.create();
-
-
-// import Resolver from 'resolver';
-
-// export default Ember.Application.extend({
-//   LOG_ACTIVE_GENERATION: false,
-//   LOG_MODULE_RESOLVER: false,
-//   LOG_TRANSITIONS: false,
-//   LOG_TRANSITIONS_INTERNAL: false,
-//   LOG_VIEW_LOOKUPS: false,
-//   modulePrefix: 'collegedesis', // TODO: loaded via config
-//   Resolver: Resolver.default,
-
-//   ready: function() {
-//     if (location.hash) {
-//       window.location = location.origin + location.hash.slice(1);
-//     }
-//   }
-// });
-
-// // register helper
-// import markdown from 'collegedesis/helpers/markdown-text';
-// Ember.Handlebars.registerHelper('markdown', markdown);
-
-// Ember.RSVP.configure('onerror', function(error) {
-//   // ensure unhandled promises raise awareness.
-//   // may result in false negatives, but visibility is more important
-//   if (error instanceof Error) {
-//     Ember.Logger.assert(false, error);
-//     Ember.Logger.error(error.stack);
-//   }
-// });
