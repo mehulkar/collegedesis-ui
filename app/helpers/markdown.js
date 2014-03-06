80c797d9973761b5c194f9e78f335700eeae7b6a
mehulkar <mehul.kar@gmail.com>
2014-03-06 00:15:29 -0800

    Fix markdown helper

--- app/helpers/markdown.js
+++ app/helpers/markdown.js
@@ -1,4 +1,4 @@
-export default Ember.Handlebars.makeBoundHelper(function(value) {
+Ember.Handlebars.helper('markdown', function(value) {
   if (Ember.isEmpty(value)) return "";
   var converter = new Showdown.converter({ extensions: ['youtube'] });
   return new Ember.Handlebars.SafeString(converter.makeHtml(value));
